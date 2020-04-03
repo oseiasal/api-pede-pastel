922626c11d3849815363deecf5fa1499a830b933
oseias <oseiasal@gmail.com>
2020-04-03 13:45:52 -0300

    ready

--- serve.js
+++ serve.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`O servidor foi iniciado na porta ${port}`);
+    console.log(`O servidor foi iniciado na porta `);
     MongoClient.connect(CONNECTION_URL, { useUnifiedTopology: true }, (error, client) => {
         if (error) {
             throw error
